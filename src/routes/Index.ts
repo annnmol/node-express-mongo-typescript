404e3e1756ad99a1512e42aeb0bb72beb639d7f2
Anmol Tanwar <anmol.tanwar@srvmedia.com>
2022-12-12 18:28:09 +0530

    homepage msg changed

--- src/routes/Index.ts
+++ src/routes/Index.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import STATUS_CODES from "../config/statusCodes";
 const router = express.Router();
 
 import authorRoutes from './authorRoutes';
@@ -7,6 +8,12 @@ import bookRoutes from "./bookRoutes";
 // split up route handling
 router.use("/authors", authorRoutes);
 router.use("/books", bookRoutes);
+//*homepage
+router.get('', (req: Request, res: Response, next: NextFunction) => {
+
+      return res.status(STATUS_CODES.SUCCESS_200).json({message:'Welcome to Node app made using Express Typescript with Mongo database. visit /authors to start'});
+    
+  });
 
 export default router;
 
